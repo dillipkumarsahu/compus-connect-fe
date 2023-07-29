25f35166cd88745c908907f7bba0308825409078
Dillip Kumar Sahu <sahudillipkumar466@gmail.com>
2023-07-29 18:29:16 +0530

    initial setup files added

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import { ConfigProvider } from "antd";
 import { theme } from "./app/config/theme";
-
+// repo changed
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
